30a74832ac40d0d61ceb0e06452c0515ac43b51f
kevin-corespace <kevin.prog@outlook.com>
2025-01-17 09:02:23 -0800

    Jan 17th init

---
+++ src/app/page.tsx
@@ -0,0 +1,9 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div className="w-screen h-screen flex justify-center items-center">
+      <div className="w-[2/3] h-screen flex justify-center items-center"></div>
+    </div>
+  );
+}
